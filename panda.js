58b67f8d3779adfdb1a8d48b75aea9f3356d6cd8
Annakluz <anna.luz.duran@gmail.com>
2017-04-18 20:45:34 -0500

    texts & buttons ready!

---
+++ panda.js
@@ -0,0 +1,27 @@
+let extincion = document.getElementById("extincion");
+let extincionText = document.getElementById("extitext");
+let origen = document.getElementById("origen");
+let origenText = document.getElementById("origentex")
+
+extincion.addEventListener("click", hideExtincion);
+origen.addEventListener("click", hideOrigen);
+
+
+
+
+function hideExtincion() {
+    if (extincionText && extincionText.style.display == "none"){
+        extincionText.style.display = "block";
+    }
+    else{
+      extincionText.style.display = "none" ; 
+    }
+    }
+function hideOrigen() {
+    if (origenText && origenText.style.display == "none"){
+        origenText.style.display = "block";
+    }
+    else{
+      origenText.style.display = "none"; 
+    }
+    }
